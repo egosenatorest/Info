5336512dd09c4c5485eb8e698afb628b2b73d9dd
egosenatorest <kingomarda@gmail.com>
2025-06-22 23:50:13 +0200

    Add files via upload

---
+++ js/ui.js
@@ -0,0 +1,12 @@
+
+
+function adminzei() {
+    document.getElementById('adminberich').style.display = 'block';
+}
+
+function hideAdminPanel() {
+    document.getElementById('adminberich').style.display = 'none';
+}
+
+window.adminzei = adminzei;
+window.hideAdminPanel = hideAdminPanel;
